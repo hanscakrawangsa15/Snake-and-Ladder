3b9c5735cc7ad98b5a19ed5f42ce5e4ffe042da6
Hans Christian Cakrawangsa <95537412+hanscakrawangsa15@users.noreply.github.com>
2024-06-28 22:40:29 +0700

    Update SoundPlayer.java

--- Snake and Ladder/src/SoundPlayer.java
+++ Snake and Ladder/src/SoundPlayer.java
@@ -1,3 +1,17 @@
+/**
+ * -----------------------------------------------------
+ * ES234211 - Programming Fundamental
+ * Genap - 2023/2024
+ * Group Capstone Project: Snake and Ladder Game
+ * -----------------------------------------------------
+ * Class    : D
+ * Group    : 05
+ * Members  :
+ * 1. 5026231130 - Hans Christian Cakrawangsa  
+ * 2. 5026231145 - Abrorus Shobah
+ * 3. 5026231161 - Muhammad Daniel Alfarisi 
+ * ------------------------------------------------------
+ */
 import javax.sound.sampled.*;
 import java.io.File;
 import java.io.IOException;
